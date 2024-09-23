fe6276a8fef0cd498565addde3235225e7833a21
Julie Van Kirk <jule.van.kirk@gmail.com>
2024-09-23 18:37:19 -0500

    feat: add say hello to julie

--- source/say_hello.py
+++ source/say_hello.py
@@ -1,8 +1,12 @@
 def say_hello():
     print("Hello, World!")
 
+def say_hello_to_julie():
+    print("Hello, Julie!")
+
 def main():
     say_hello()
+    say_hello_to_julie()
 
 if __name__ == "__main__":
     main()
